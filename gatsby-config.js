58d4122ba77dabf48d96178c801282fedb013d48
ss4223 <>
2020-08-14 22:28:53 +0900

    add plugin settings Contentful, helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,21 @@
-module.exports = {
-    plugins: [
+require("dotenv").config();
 
-    ]
-}
+module.exports = {
+  plugins: [
+    // helmet
+    `gatsby-plugin-react-helmet`,
+    // Contentful settings
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.SPACE_ID,
+        accessToken: process.env.ACCESS_TOKEN,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: "札幌情報デスク",
+    description: "エレクトロスフィア道中",
+    author: "@dlsakai",
+  },
+};
