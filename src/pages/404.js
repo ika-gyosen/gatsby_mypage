7c0691c0494204156ccff140b80ae44d1cf4405c
ss4223 <>
2020-08-15 00:47:15 +0900

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class IndexPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>sorry 404 not fond</h1>
+      </div>
+    );
+  }
+}
