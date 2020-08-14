2ea91bb4198c6d3338c24ca8eee79360554a029d
ss4223 <>
2020-08-14 23:37:19 +0900

    add markdown for contentful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,46 @@ module.exports = {
         accessToken: process.env.ACCESS_TOKEN,
       },
     },
+    // filesystem
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/md/`,
+      },
+    },
+    // remark and prism settings
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: true,
+              noInlineHighlight: false,
+              languageExtensions: [
+                {
+                  language: "superscript",
+                  extend: "javascript",
+                  definition: {
+                    superscript_types: /(SuperType)/,
+                  },
+                  insertBefore: {
+                    function: {
+                      superscript_keywords: /(superif|superelse)/,
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
   ],
   siteMetadata: {
     title: "札幌情報デスク",
