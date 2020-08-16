df05181deb393f5b60be961596e93a1d20a4e1c7
ss4223 <>
2020-08-16 23:50:33 +0900

    add blog post list page

---
+++ src/pages/blogpost.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export default class Blogposts extends React.Component {
+  render() {
+    console.log(this.props.data.allContentfulBlogPost.edges);
+    return (
+      <ul>
+        {this.props.data.allContentfulBlogPost.edges.map(({ node }) => {
+          return (
+            <li>
+              <Link to={`/blogpost/${node.slug}`}>{node.title}</Link>
+              {"  "} <ShowDate datevalue={node.createdAt} />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export const PageQuery = graphql`
+  query {
+    allContentfulBlogPost {
+      edges {
+        node {
+          title
+          createdAt
+          slug
+        }
+      }
+    }
+  }
+`;
+
+function ShowDate(props) {
+  const dateTime = new Date(Date.parse(props.datevalue));
+  const weekTable = ["月", "火", "水", "木", "金", "土", "日"];
+  return (
+    <span>
+      {dateTime.getFullYear()}年{dateTime.getMonth()}月{dateTime.getDate()}日
+      {" ("}
+      {weekTable[dateTime.getDay()]}
+      {") "} {dateTime.getHours()}:{dateTime.getMinutes()}:
+      {dateTime.getSeconds()}
+    </span>
+  );
+}
